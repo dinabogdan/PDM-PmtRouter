e6265c6ea34dbdeb6ba5aff42e174a2a004314c6
dinabogdan <bogdan.dina03@gmail.com>
2018-03-11 21:54:36 +0200

    commit for project structure

---
+++ PDM-PmtRouter/src/main/java/org/pdm/ib/pmt/router/PdmPmtRouterApplication.java
@@ -0,0 +1,12 @@
+package org.pdm.ib.pmt.router;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PdmPmtRouterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PdmPmtRouterApplication.class, args);
+	}
+}
---
+++ PDM-PmtRouter/src/test/java/java/org/pdm/ib/pmt/router/PDMPmtRouter/PdmPmtRouterApplicationTests.java
@@ -0,0 +1,16 @@
+package java.org.pdm.ib.pmt.router.PDMPmtRouter;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PdmPmtRouterApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
